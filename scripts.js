0ac7cff891a3c2abe08f0ff30e24b8a1becd59dd
Ruhi Parween <parweenruhi912@gmail.com>
2024-10-10 06:03:47 +0530

    initial files added

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer", "Open Source Enthusiast", "Blogger"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay: 1000,
+    loop: true
+})
